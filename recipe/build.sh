d6b72682d69faade3caf62bf96d1083153a2ee04
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-08-21 14:05:43 -0500

    ENH: check build shared lib for problematic undefined symbol

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 cp $RECIPE_DIR/make.inc .
 cp $RECIPE_DIR/makefile ./SRC/.
- 
+
 cd SRC/
 make single_double_complex_dcomplex
 
@@ -10,7 +10,7 @@ mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 
 if [[ "$target_platform" == linux-* ]]; then
-  $FC  -shared -o liblapack95.so $LDFLAGS -Wl,--whole-archive lapack95.a -Wl,--no-whole-archive $PREFIX/lib/liblapack.so $PREFIX/lib/libblas.so
+  $FC -shared -o liblapack95.so $LDFLAGS -Wl,--whole-archive lapack95.a -Wl,--no-whole-archive $PREFIX/lib/liblapack.so $PREFIX/lib/libblas.so
 elif [[ "$target_platform" == osx-* ]]; then
   $FC -shared -o liblapack95.dylib $LDFLAGS -Wl,-undefined -Wl,dynamic_lookup -Wl,-all_load lapack95.a -Wl,-noall_load $PREFIX/lib/liblapack.dylib $PREFIX/lib/libblas.dylib
 fi
@@ -20,3 +20,8 @@ cp liblapack95$SHLIB_EXT $PREFIX/lib/liblapack95$SHLIB_EXT
 cp lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
+
+if "$NM" -u liblapack95$SHLIB_EXT | grep _clagge_; then
+  echo "_clagge_ found :("
+  exit 1
+fi
